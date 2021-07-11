0774583879ba44f81e52fef7635042a921223268
edmilson-dk <droidvidaboa@gmail.com>
2021-07-11 12:00:25 -0300

    feat: created constants object

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,3 +1,10 @@
+import { commands } from "./commands";
+import { fullConfigFile } from "./configs";
+import { messages } from "./messages";
+
 export const CONSTANTS = {
-  fileConfigsNameDefault: "scc.config.json",
-}
+  fileConfigsNameDefault: "scc.configs.json",
+  commands,
+  fullConfigFile,
+  messages
+};
