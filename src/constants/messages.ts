bdc3e6b92e74f1d86bd8080dbf1528fce1bc6727
edmilson-dk <droidvidaboa@gmail.com>
2021-07-13 20:24:34 -0300

    feat: created new message

--- src/constants/messages.ts
+++ src/constants/messages.ts
@@ -1,3 +1,4 @@
 export const messages = {
-  configsFileCreated: "\n✅ - Configs file created with successfully\n"
+  configsFileCreated: "\n✅ - Configs file created with successfully\n",
+  configsFileNotCreated: "\n❌ No configuration file found, use 'scc init' to create one.\n",
 }
