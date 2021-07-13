5b25982339ef642953067e869a4478c4791eaf08
edmilson-dk <droidvidaboa@gmail.com>
2021-07-13 20:27:07 -0300

    feat: created get scc configs or null

--- src/helpers/index.ts
+++ src/helpers/index.ts
@@ -1,14 +1,25 @@
-import { promises } from "fs";
-import { mapperFileConfigs } from "../shared/mappers/config";
+import { promises, existsSync } from "fs";
 
+import { mapperFileConfigs } from "../shared/mappers/config";
 import { FileConfigPropsType } from "../shared/types/config";
 
-export async function parseConfigsFile(jsonPath: string, fileName: string) {
-  const json = await promises.readdir(`${jsonPath}`);
+export async function parseConfigsFile(filePath: string, fileName: string) {
+  const json = await promises.readdir(`${filePath}`);
 
   if(json.includes(fileName)) {
-    const jsonFile: FileConfigPropsType = require(`${jsonPath}/${fileName}`);
+    const jsonFile: FileConfigPropsType = require(`${filePath}/${fileName}`);
     const mappedConfigs = mapperFileConfigs(jsonFile);
     return mappedConfigs;
   }
+}
+
+export async function getSccConfigsOrNull() {
+  const existsFile = existsSync(`${__dirname}/../../scc.configs.json`);
+
+  if(existsFile) {
+    const mappedConfigs = await parseConfigsFile(`${__dirname}/../../`, "scc.configs.json");
+    return mappedConfigs;
+  }
+
+  return null;
 }
