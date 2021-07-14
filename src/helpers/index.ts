da57bf777d579f6bb0eb8f80301a94e3e8d1805e
edmilson-dk <droidvidaboa@gmail.com>
2021-07-13 21:48:20 -0300

    feat: created handle create configs folders

--- src/helpers/index.ts
+++ src/helpers/index.ts
@@ -1,7 +1,15 @@
-import { existsSync } from "fs";
+import { existsSync, promises } from "fs";
 
 import { mapperFileConfigs } from "../shared/mappers/config";
 import { FileConfigPropsType } from "../shared/types/config";
+
+export function handleCreateConfigsFolders(foldersPaths: string[]) {
+  foldersPaths.forEach(async folderPath => {
+    if(!existsSync(folderPath)) {
+      await promises.mkdir(folderPath);
+    }
+  });
+}
 
 export async function getSccConfigsOrNull(): Promise<FileConfigPropsType | null> {
   const existsFile = existsSync(`${__dirname}/../../scc.configs.json`);
