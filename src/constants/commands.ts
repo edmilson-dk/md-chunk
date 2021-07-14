8e8e52865066f3f0300adf1729877234caca30db
edmilson-dk <droidvidaboa@gmail.com>
2021-07-13 21:47:55 -0300

    feat: added news messages

--- src/constants/commands.ts
+++ src/constants/commands.ts
@@ -1,3 +1,4 @@
 export const commands = {
   initCommandDescription: "initialize scc json configs file",
+  convertAll: "convert all markdown files to HTML in input directory"
 }
--- src/constants/messages.ts
+++ src/constants/messages.ts
@@ -1,4 +1,5 @@
 export const messages = {
   configsFileCreated: "\n✅ - Configs file created with successfully\n",
-  configsFileNotCreated: "\n❌ No configuration file found, use 'scc init' to create one.\n",
+  configsFileNotCreated: "\n❌ No configuration file found, use 'scc init' to create one\n",
+  convertAllSuccess: "\n 🔄 - All markdown files converted to HTML\n",
 }
