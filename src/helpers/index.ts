d6f2bd5e0efd7a00be12a6ec683298cc5715e3a6
edmilson-dk <droidvidaboa@gmail.com>
2021-07-16 11:45:48 -0300

    feat: created render html base template helper function

--- src/helpers/index.ts
+++ src/helpers/index.ts
@@ -6,7 +6,7 @@ import { FileConfigPropsType } from "../shared/types/config";
 
 export function handleCreateConfigsFolders(foldersPaths: string[]) {
   foldersPaths.forEach(async folderPath => {
-    if(!existsSync(folderPath)) {
+    if (!existsSync(folderPath)) {
       await promises.mkdir(folderPath);
     }
   });
@@ -15,7 +15,7 @@ export function handleCreateConfigsFolders(foldersPaths: string[]) {
 export async function getSccConfigsOrNull(): Promise<FileConfigPropsType | null> {
   const existsFile = existsSync(`${__dirname}/../../scc.configs.json`);
 
-  if(existsFile) {
+  if (existsFile) {
     const jsonFile: FileConfigPropsType = require(`${__dirname}/../../scc.configs.json`);
     const mappedConfigs = mapperFileConfigs(jsonFile);
     return mappedConfigs;
@@ -25,10 +25,39 @@ export async function getSccConfigsOrNull(): Promise<FileConfigPropsType | null>
 }
 
 export function validatePath(targetPath: string) {
-  if(!existsSync(targetPath)) {
+  if (!existsSync(targetPath)) {
     console.log(
       chalk.red(`\n❌ - Path does not exist: ${targetPath}, execute 'scc setup' to create setup tool\n`)
     );
     process.exit(1);
   }
+}
+
+export function renderHtmlBaseTemplate(targetHtml: string) {
+  return `
+    <!DOCTYPE html>
+    <html>
+      <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <title>SCC</title>
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.1.0/styles/base16/snazzy.min.css">
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.1.0/highlight.min.js"></script>
+        <!-- and it's easy to individually load additional languages -->
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.1.0/languages/go.min.js"></script>
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+        <style>
+          .hljs {
+            padding: 20px;
+            border-radius: 10px;
+          }
+        </style>
+      </head>
+      <body>
+        <div class="container">
+          ${targetHtml}
+        </div>
+      </body>
+    </html>
+  `;
 }
