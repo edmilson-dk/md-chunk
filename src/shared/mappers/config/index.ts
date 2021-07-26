63f25f171826f76420a95faec0f9f732cfefe150
edmilson-dk <droidvidaboa@gmail.com>
2021-07-26 18:25:58 -0300

    feat: added new mapper property

--- src/shared/mappers/config/index.ts
+++ src/shared/mappers/config/index.ts
@@ -4,6 +4,7 @@ export function mapperFileConfigs(fileConfigs: any): FileConfigPropsType {
   return {
     inputMarkdown: fileConfigs.inputMarkdown,
     outputHTML: fileConfigs.outputHTML,
+    inputBaseHTML: fileConfigs.inputBaseHTML,
     deleteInput: fileConfigs.deleteInput,
     originalName: fileConfigs.originalName,
     filePrefix: fileConfigs.filePrefix,
