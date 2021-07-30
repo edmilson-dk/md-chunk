6a982e2bac98899a7bbaff2182f6c331d25cb2c7
edmilson-dk <droidvidaboa@gmail.com>
2021-07-29 22:12:09 -0300

    feat: created initial converts for html configs types

--- src/core/converts.ts
+++ src/core/converts.ts
@@ -4,7 +4,7 @@ import remark from "remark";
 import html from "remark-html";
 import { randomBytes } from "crypto";
 
-import { convertAllMarkdownFilesToHtmlProps } from "../shared/types/core/converts";
+import { convertAllMarkdownFilesToHtmlNoBaseHtmlProps, convertAllMarkdownFilesToHtmlProps, convertAllMarkdownFilesToHtmlWithBaseHtmlProps } from "../shared/types/core/converts";
 
 export function convertMarkdownFileToHtml(content: string): string {
   const convertedContent = remark()
@@ -15,11 +15,61 @@ export function convertMarkdownFileToHtml(content: string): string {
   return convertedContent;
 }
 
+function convertMarkdownFileToHtmlNotBaseHtml({
+  inputPath,
+  file,
+  originalName,
+  outputPath,
+  filePrefix,
+}: convertAllMarkdownFilesToHtmlNoBaseHtmlProps) {
+  let id = randomBytes(16).toString("hex");
+  const filePath = path.join(inputPath, file);
+
+  const saveFileName = originalName
+    ? file.replace(".md", ".html")
+    : `${id}.html`;
+
+  const saveFilePath = path.join(outputPath, `${filePrefix}${saveFileName}`);
+  const fileContent = fs.readFileSync(filePath, "utf8");
+  const convertedContent = convertMarkdownFileToHtml(fileContent);
+
+  // fs.writeFileSync(saveFilePath, convertedContent);
+}
+
+function convertMarkdownFileToHtmlWithBaseHtml({
+  inputPath,
+  file,
+  originalName,
+  outputPath,
+  filePrefix,
+  baseInputPath
+}: convertAllMarkdownFilesToHtmlWithBaseHtmlProps) {
+  let id = randomBytes(16).toString("hex");
+  const filePath = path.join(inputPath, file);
+
+  const saveFileName = originalName
+    ? file.replace(".md", ".html")
+    : `${id}.html`;
+
+  const saveFilePath = path.join(outputPath, `${filePrefix}${saveFileName}`);
+  const baseHtmlPath = path.join(baseInputPath, "base.html");
+
+  const baseHtmlContent = fs.readFileSync(baseHtmlPath, "utf8");
+  const fileContent = fs.readFileSync(filePath, "utf8");
+
+  const convertedContent = convertMarkdownFileToHtml(fileContent);
+  const baseHtmlConvertedContent = baseHtmlContent.replace(/<!--\s*BASE_HTML_CONTENT_PLACEHOLDER\s*-->/g, convertedContent);
+  console.log(baseHtmlConvertedContent);
+  // fs.writeFileSync(saveFilePath, convertedContent);
+}
+
 export async function convertAllMarkdownFilesToHtml({
   inputPath,
   outputPath,
   originalName = true,
-  filePrefix = ""
+  filePrefix = "",
+  useBaseHTML = false,
+  baseInputPath = ""
 }: convertAllMarkdownFilesToHtmlProps) {
   if (!fs.existsSync(outputPath)) {
     throw Error(`Input path ${inputPath} does not exist, execute "scc setup" for setup tool`);
@@ -29,18 +79,27 @@ export async function convertAllMarkdownFilesToHtml({
 
   allFiles.forEach((file) => {
     if (file.slice(-3) === ".md") {
-      let id = randomBytes(16).toString("hex");
-      const filePath = path.join(inputPath, file);
-
-      const saveFileName = originalName
-        ? file.replace(".md", ".html")
-        : `${id}.html`;
-
-      const saveFilePath = path.join(outputPath, `${filePrefix}${saveFileName}`);
-      const fileContent = fs.readFileSync(filePath, "utf8");
-      const convertedContent = convertMarkdownFileToHtml(fileContent);
-
-      fs.writeFileSync(saveFilePath, convertedContent);
+      switch (useBaseHTML) {
+        case true:
+          convertMarkdownFileToHtmlWithBaseHtml({
+            inputPath,
+            file,
+            originalName,
+            outputPath,
+            filePrefix,
+            baseInputPath
+          });
+          break;
+        case false:
+          convertMarkdownFileToHtmlNotBaseHtml({
+            inputPath,
+            file,
+            originalName,
+            outputPath,
+            filePrefix,
+          });
+          break;
+      }
     }
   });
 }
