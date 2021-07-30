e887ff7a75b9bfd6a87b46b0eeee94c8952e37d5
edmilson-dk <droidvidaboa@gmail.com>
2021-07-29 22:12:23 -0300

    feat: created news types

--- src/shared/types/core/converts.ts
+++ src/shared/types/core/converts.ts
@@ -3,4 +3,21 @@ export type convertAllMarkdownFilesToHtmlProps = {
   outputPath: string;
   originalName: boolean;
   filePrefix: string;
-}
+  useBaseHTML: boolean;
+  baseInputPath: string;
+};
+
+export type convertAllMarkdownFilesToHtmlNoBaseHtmlProps = Omit<
+  convertAllMarkdownFilesToHtmlProps,
+  "useBaseHTML" | "baseInputPath"
+> & {
+  file: string;
+};
+
+export type convertAllMarkdownFilesToHtmlWithBaseHtmlProps = Omit<
+  convertAllMarkdownFilesToHtmlProps,
+  "useBaseHTML" | "baseInputPath"
+> & {
+  file: string;
+  baseInputPath: string;
+};
